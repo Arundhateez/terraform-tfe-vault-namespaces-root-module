ad6fee16bdbab2d0593a75b3e77c4f53409dccd1
Simon Elliott <33635580+sce81@users.noreply.github.com>
2025-10-24 17:38:43 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,20 @@
-variable "namespace" {
-    type = string
-    default = null
-    description = "Name of Namespace to deploy"
+variable "vault_url" {
+  type        = string
+  default     = null
+  description = "URL of Vault to configure"
 }
-
+variable "vault_namespace" {
+  type        = string
+  default     = null
+  description = "Namespace in which to deploy configuration"
+}
+variable "namespace" {
+  type        = string
+  default     = null
+  description = "Name of Namespace to deploy"
+}
 variable "child_namespaces" {
-    type = set(string)
-    default = []
-    description = "Name of Child Namespaces to deploy"
+  type        = string
+  default     = null
+  description = "Name of Namespace to deploy"
 }
