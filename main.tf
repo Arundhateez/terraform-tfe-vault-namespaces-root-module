01a81b8205b4242602f8cad0413511e5bf2b65e7
Arundhateez <52918421+Arundhateez@users.noreply.github.com>
2025-11-13 11:12:59 +0000

    Update Vault module source to LegalGeneral-OneTech

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vault-namespaces" {
-  source = "app.terraform.io/HashiCorp_AWS_Org/terraform-vault-deploy-namespaces/vault"
+  source = "app.terraform.io/LegalGeneral-OneTech/terraform-vault-deploy-namespaces/vault"
 
   namespace        = var.namespace
   child_namespaces = toset(split(", ", var.child_namespaces))
